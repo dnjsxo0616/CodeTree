9725a7a7175d4f2d82326f1b5c5b2b4855ca8e76
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2025-02-18 19:16:54 +0900

    Title: 함수를 이용한 369 게임, Time: 5093ms, Memory: 16MB, Status: Time Limit Exceed - CodeTree

--- 250218/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250218/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,11 +1,22 @@
 a, b = map(int, input().split())
 
 cnt = 0
-for i in range(a, b+1):
-    num1 = i//10
-    num2 = i%10
-    if (num1==3 or num1==6 or num1==9) or (num2==3or num2==6or num2==9) or i%3==0:
-        cnt+=1
+
+def contains(n):
+    while n>0:
+        if n%10==3 or n%10==6 or n%10==9:
+            return True
+        n//10
+
+    return False
+
+def is_number(n):
+    return contains(n) or (n%3==0)
 # Write your code here!
+
+cnt=0
+for i in range(a, b+1):
+    if is_number(i):
+        cnt+=1
 
 print(cnt)
